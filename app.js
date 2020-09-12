c6d40dd585c0425c1e339e75a198a2d4c6d8475b
jobwow <da.hagnere@gmail.com>
2020-09-12 21:49:36 +0200

    downgrade node to test old routes

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const session = require("express-sessions");
-const expressValidator = require("express-validator");
-
-
-//port
-const port = 5000;
-
-//init app
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+// Port
+const port = 3000;
+// init app
 const app = express();
 
-//View setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view-engine', 'pug');
+const index = require('./routes/index');
+const articles = require('./routes/articles');
+const categories= require('./routes/categories');
+const manage = require('./routes/manage');
 
-//Body-parser Middleware
+// View Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Public static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//express messages middleware
+// Express messages
 app.use(require('connect-flash')());
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.messages = require('express-messages')(req, res);
   next();
 });
 
+// Express validator
+app.use(expressValidator({
+  errorFormatter: (param, msg, value) => {
+      const namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-app.listen(port , (req, res, next) => {
-  console.log("server running on port " +port)
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use('/', index);
+app.use('/articles', articles);
+app.use('/categories', categories);
+app.use('/manage', manage);
+
+app.listen(port, () => {
+  console.log('Server started on port '+port);
 });
